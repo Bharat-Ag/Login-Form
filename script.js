609fc6f5a2a3906b29cbfc088fc3a55d4bb6e2fe
Bharat-Ag <108234716+Bharat-Ag@users.noreply.github.com>
2022-12-06 00:27:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+let container = document.querySelector(".container"),
+    showHide = document.querySelectorAll(".showHide"),
+    pwField = document.querySelectorAll(".password"),
+    signUp = document.querySelector(".signup-link"),
+    login = document.querySelector(".login-link");
+
+
+
+showHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener('click', () => {
+        pwField.forEach(pwField => {
+            if (pwField.type === "password") {
+                pwField.type = "text";
+
+
+
+                showHide.forEach(icon => {
+                    icon.classList.replace("bx-low-vision", "bx-show");
+                })
+            } else {
+                pwField.type = "password";
+
+                showHide.forEach(icon => {
+                    icon.classList.replace("bx-show", "bx-low-vision");
+                })
+
+            }
+        })
+    })
+})
+
+signUp.addEventListener('click',()=>{
+    container.classList.add("active")
+})
+login.addEventListener('click',()=>{
+    container.classList.remove("active")
+})
